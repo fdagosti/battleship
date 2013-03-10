d4523c0f20018d8f961c037f0fbcf15ffd917429
Francois D'Agostini <fdagosti@nds.com>
2013-03-10 17:50:26 +0100

    added the finding algorithm

--- battleship.py
+++ battleship.py
@@ -7,7 +7,19 @@ def printBoard(board):
 def addHiddenShip(board):
 	randRow = randrange(0, len(board))
 	randCol = randrange(0, len(board[0]))
-
+	
 	board[randRow][randCol] = 'X'
-	print(randRow,randCol)
-	printBoard(board)
+	
+
+def hasShipBeenFound(board, row, col):
+	if (0 > row >= len(board)) or (0 > col >= len(board[0])):
+		print("You are out of the ocean !!")
+		return False
+	elif board[row][col] == 'X':
+		print("Yeah, Good job")
+		return True
+	else:
+		board[row][col] = 'M'
+		print("Missed")
+		printBoard(board)
+		return False
--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 import battleship
 
-boardRow = ['O','O','O','O','O']
-board = [list(boardRow),list(boardRow),list(boardRow),list(boardRow),list(boardRow)]
+def launchGame():
 
-print("------- BattleShip V0.1 --------")
+	boardRow = list('OOOOO')
+	board = [list(boardRow),list(boardRow),list(boardRow),list(boardRow),list(boardRow)]
 
-battleship.printBoard(board)
+	print("------- BattleShip V0.1 --------")
 
-print("\n------- adding a hidden ship ----")
-battleship.addHiddenShip(board)
+
+
+	print("\n------- adding a hidden ship ----")
+	battleship.addHiddenShip(board)
+	battleship.printBoard(board)
+
+	
+	tries = 3
+
+	while tries >0:
+		row = int(input("Ship's row? : "))
+		col = int(input("Ship's Col? : "))
+
+		result = battleship.hasShipBeenFound(board, row, col)
+		
+		if (result):
+			break
+		tries -=1
+
+	if (tries >0):
+		print("Congratulations, you found the ship")
+	else:
+		print("Sorry, ship not found")
